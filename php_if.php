171c6e0b59e397a4bbb20929746aa0bd87669c6d
Yasmina1602 <147608831+Yasmina1602@users.noreply.github.com>
2024-04-10 13:43:42 +0500

    php

--- php_if.php
+++ php_if.php
@@ -65,4 +65,86 @@
     $b = 55;
     $a == $b ? $a = $b = $a + $b : $a = $b = 0;
     echo "\n if 10-> result = ".$a.", ".$b;
+    
+        // if 11
+    $a = 11;
+    $b = 16;
+    $a != $b ? $a = $b = max($a, $b) : $a = $b = 0;
+    echo "\nif 11 -> a = ".$a.", b = ".$b;
+        
+        // if 12
+    $a = 21;
+    $b = 16;  
+    $c = 60;  
+    echo "\nif 12 -> min = ".min($a, $b, $c);
+    
+        // if 13
+    $a = [21, 1, 10];
+    sort($a);
+    echo "\nif 13 -> medium = ".$a[1];
+    
+        // if 14
+    $a = 21;
+    $b = 16;  
+    $c = 60;  
+    echo "\nif 14 -> min = ".min($a, $b, $c).", max = ".max($a, $b, $c);
+    
+        // if 15
+    $a = [111, 22, 30];
+    sort($a);
+    echo "\nif 15 -> max1 + max2 = ".$a[2] + $a[1];
+    
+        // if 16
+    $a = 1;
+    $b = 16;  
+    $c = 60;
+    if ($a < $b && $b < $c) { 
+        $a *= 2;
+        $b *= 2;
+        $c *= 2;
+    } else {
+        $a *= -1;
+        $b *= -1;
+        $c *= -1;
+    }
+    echo "\nif 16 -> a = ".$a.", b = ".$b.", c = ".$c;
+    
+        // if 17
+    $a = 21;
+    $b = 16;  
+    $c = 60;
+    if (($a < $b && $b < $c)||($a > $b && $b > $c)) { 
+        $a *= 2;
+        $b *= 2;
+        $c *= 2;
+    } else {
+        $a *= -1;
+        $b *= -1;
+        $c *= -1;
+    }
+    echo "\nif 17 -> a = ".$a.", b = ".$b.", c = ".$c;
+    
+        // if 18
+    $a = 21;
+    $b = 16;  
+    $c = 16;
+    echo "\nif 18 -> result = ";
+    if($a == $b) echo $c;
+    if($a == $c) echo $b;
+    if($c == $b) echo $a;
+
+        // if 19
+    $a = [12, 66, 12, 12];
+    sort($a);
+    echo "\nif 19 -> result = ";
+    echo $a[0] == $a[1] ? $a[3] : $a[0];
+        
+        // if 20
+    $a = [12, 0];
+    $b = [20, 11];
+    $c = [0, 0];
+    $b = sqrt(pow(($a[0] - $b[0]),2) + pow(($a[1] - $b[1]),2));
+    $c = sqrt(pow(($a[0] - $c[0]),2) + pow(($a[1] - $c[1]),2));
+    echo "\nif 20 -> ";
+    echo $b < $c ? "b = ".$b : "c = ".$c;
 ?>
